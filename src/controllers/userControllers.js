d5e631fdf5564c9d7a7c48b90d93b75ea82df690
emilmagdy <dremilmagdy@gmail.com>
2026-08-07 01:18:12 +0300

    test

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,16 +1,22 @@
 import prisma from '../generated/prisma/client.js'
+
 import bcrypt from 'bcrypt'
+
 export const userRegistration = async (req, res) => {
+    try{
+        
     const { name, email, password } = req.body
     // check if the user exists in the db
     const userExists = await prisma.user.findUnique({
         where: { email: email }
     })
-    if (user) {
+    if (userExists) {
         return res.json({ "message": "this email is already registered please login" })
     }
     return res.json({ "message": "still not finished" })
-}
+}   catch (err) {
+return res.status(500).json({"error":"error occured"})
+}}
 
 export const userLogin = (req, res) => {
     return res.json({ "message": "This is the login route" })
