ebe863f1e4939a0586e9a2398ad1509de2ee89f7
emilmagdy <dremilmagdy@gmail.com>
2026-08-23 00:09:14 +0300

    Removes the dist folder from the git cahce and added this to gitignore

--- dist/utils/AppError.js
+++
@@ -1,8 +0,0 @@
-class AppError extends Error {
-    statusCode;
-    constructor(message, statusCode) {
-        super(message);
-        this.statusCode = statusCode;
-    }
-}
-export default AppError;
